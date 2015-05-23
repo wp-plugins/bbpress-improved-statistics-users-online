73bea63e364c52651a9da907a2d9762d0fbf9973
GeekServe <GeekServe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-23 22:37:14 +0000

    Bugfix for 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/bbpress-improved-statistics-users-online/tags/1.0.2.1@1166438 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bbpress-advanced-statistics.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Plugin Name: bbPress Advanced Statistics
+ * Version: 1.0.2.1
+ * Plugin URI: http://www.thegeek.info
+ * Description: Advanced Statistics Available for bbPress users, introducing a familiar looking online and statistics section
+ * Author: Jake Hall
+ * Author URI: http://www.thegeek.info
+ * Requires at least: 3.9
+ * Tested up to: 4.2.1
+ *
+ * @package WordPress
+ * @author Jake Hall
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Load plugin class files
+require_once( 'includes/class-bbpress-advanced-statistics.php' );
+require_once( 'includes/class-bbpress-advanced-statistics-settings.php' );
+require_once( 'includes/class-bbpress-advanced-statistics-online.php' );
+
+// Load plugin libraries
+require_once( 'includes/lib/class-bbpress-advanced-statistics-admin-api.php' );
+
+/**
+ * Returns the main instance of bbPress_Advanced_Statistics to prevent the need to use globals.
+ *
+ * @since  1.0.0
+ * @return object bbPress_Advanced_Statistics
+ */
+function bbPress_Advanced_Statistics () {
+    $instance = bbPress_Advanced_Statistics::instance( __FILE__, '1.0.2' );
+
+    if ( is_null( $instance->settings ) ) {
+        $instance->settings = bbPress_Advanced_Statistics_Settings::instance( $instance );
+    }
+    
+    if ( is_null( $instance->online ) ) {
+        $instance->online = bbPress_Advanced_Statistics_Online::instance( $instance );
+    }
+
+    return $instance;
+}
+
+bbPress_Advanced_Statistics();
